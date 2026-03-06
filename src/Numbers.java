e22ba0f493883aa80b225ed431291afc1be7f47f
Prince-Uche <ucheprince7@gmail.com>
2026-03-06 14:32:53 +0100

    commit

--- src/Numbers.java
+++ src/Numbers.java
@@ -61,12 +61,80 @@ public class Numbers {
     public int sumOfDigits(int number){
         int sum = 0;
         while (number % 10 != 0){
-            sum += number%10;
+            sum += number % 10;
             number /= 10;
         }
         return sum;
     }
 
 
+    public boolean isSpecial(int number){
+        ArrayList<Integer>factors = new ArrayList<>();
+        int sum = 0;
+        for(int i = 1; i < number; i++){
+            if(number % i == 0){
+                factors.add(i);
+            }
+        }
+        for(int digits:factors){
+           sum += digits;
+        }
+        return sum == number;
+
+    }
+
+    public boolean isPalindrome(int number){
+        String numberString = String.valueOf(number);
+        String reverseNumber = new StringBuilder(numberString).reverse().toString();
+        return numberString.equals(reverseNumber);
+    }
+
+
+    public int fibonacci(int number){
+        int fibonacci = 0;
+        int fibonacci1 = 0;
+        int fibonacci2 = 1;
+
+        if(number < 0){
+            throw new IllegalArgumentException("number is negative");
+        }
+        if(number == 0){
+            return 0;
+        }
+        if(number == 1){
+            return 1;
+        }
+        else{
+            for(int count = 1; count < number; count++){
+                fibonacci = fibonacci1 + fibonacci2;
+                fibonacci1 = fibonacci2;
+                fibonacci2 = fibonacci;
+            }
+            return fibonacci;
+        }
+    }
+
+
+    public int triangularNumber(int number){
+        int triangularNumber = 0;
+        int tNumber = 1;
+
+        if(number < 0){
+            throw new IllegalArgumentException("number is negative");
+        }
+        if(number == 0){
+            return 0;
+        }
+
+        else{
+            for(int count = 1; count <= number; count++){
+                triangularNumber = tNumber*(tNumber + 1) / 2;
+                tNumber = tNumber + 1;
+            }
+            return triangularNumber;
+        }
+    }
+
+    
 
 }
--- test/NumbersTest.java
+++ test/NumbersTest.java
@@ -1,10 +1,22 @@
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumbersTest {
 
+    Numbers numbers;
     @BeforeEach
     void setUp() {
+        numbers = new Numbers();
+    }
+
+    @Test
+    void test_Fibonacci() {
+        assertEquals(8, numbers.fibonacci(6));
+    }
+
+    @Test
+    void test_TriangularNumber() {
+        assertEquals(10, numbers.triangularNumber(4));
     }
 }
